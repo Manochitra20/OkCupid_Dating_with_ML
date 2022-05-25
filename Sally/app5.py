e2ce82ac5271fcaa675d13d4584300b85d8877f8
Sally Ahmed <sally.elsokary@gmail.com>
2022-05-25 14:02:14 -0700

    update

--- Sally/app5.py
+++ Sally/app5.py
@@ -2,17 +2,33 @@ from flask import Flask
 app = Flask(__name__)
 
 # Create an instance of Flask
-import dash_core_components as pip
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import plotly.express as px
 import pandas as pd
 
 
 # Route to the basic info
-@app.route('/chart')
-def chart():
-    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/...')
+@app.route('/sunburst')
+def sunburst():
+    # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/...')
+    df=pd.read_csv("OKcupid_cleaned_up.csv")
+
+    starbucks_dist = df.groupby(by=["location", "job", "offspring",]).count()[["income"]].rename(columns={"income":"Count"})
+    starbucks_dist["okcupid"] = "okcupid"
+    starbucks_dist = starbucks_dist.reset_index()
+
     fig = px.sunburst(starbucks_dist, path=["okcupid", "location", "job", "offspring", ], values='Count',
                   title="Sunburst OkCupid", width=1000, height=800)
     fig.show()(df, x='Date', y='AAPL.High')
     return  html.Div([dcc.Graph(figure=fig)])
+
+
+@app.route('/chart')
+def chart():
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/...')
+    fig = px.line(df, x='Date', y='AAPL.High')
+    return  html.Div([dcc.Graph(figure=fig)])
+    
+if __name__ == "__main__":
+    app.run()
